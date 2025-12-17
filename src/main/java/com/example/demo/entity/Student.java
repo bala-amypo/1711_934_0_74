c852ca7162c2986a93641365f87f63ab7cb08b26
bala-amypo <bala@amypo.in>
2025-12-17 06:28:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,24 +12,11 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String email;
+    private LocalDate dob;
     private float cgpa;
 
-
-    public Student() {
-    }
-
-    // Parameterized constructor
-    public Student(Long id, String name, String email, float cgpa) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.cgpa = cgpa;
-    }
-
-    // Getters and Setters
     public Long getId() {
         return id;
     }
@@ -53,11 +41,30 @@ public class Student {
         this.email = email;
     }
 
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
     public float getCgpa() {
         return cgpa;
     }
 
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
+    }
+
+    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.cgpa = cgpa;
+    }
+
+    public Student() {
     }
 }
